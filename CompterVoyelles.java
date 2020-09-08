605272dbb2ad62bf00539e185aafed7ff221b9af
damien-d-bzh <51257374+damien-d-bzh@users.noreply.github.com>
2020-09-08 09:36:58 +0200

    Add files via upload
    Un programme qui prend une chaine de caractère de l'utilisateur et va en extraire le nombre de voyelles (a, e, i, o, u, y), afficher le nombre de chaque voyelles saisies et le nombre total de voyelles saisies.

--- CompterVoyelles.java
+++ CompterVoyelles.java
@@ -1,34 +1,54 @@
 import java.util.*;
 public class CompterVoyelles {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a=0, e=0, i=0, o=0, u=0;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Entrer une phrase :");
-		String phrase=sc.nextLine();
-		for(int b=0; b<phrase.length(); ++b) {
-			if(phrase.charAt(b)=='a'||phrase.charAt(b)=='A') {
-				a+=1;
-			}
-			if(phrase.charAt(b)=='e'||phrase.charAt(b)=='E') {
-				e+=1;
-			}
-			if(phrase.charAt(b)=='i'||phrase.charAt(b)=='I') {
-				i+=1;
-			}
-			if(phrase.charAt(b)=='o'||phrase.charAt(b)=='O') {
-				o+=1;
-			}
-			if(phrase.charAt(b)=='u'||phrase.charAt(b)=='U') {
-				U+=1;
-			}
-		}
-		System.out.println("a :"+a);
-		System.out.println("e :"+e);
-		System.out.println("i :"+i);
-		System.out.println("o :"+o);
-		System.out.println("u :"+u);
-
-}
+    public static void main(String[] args){
+        Scanner cl=new Scanner(System.in);
+        int a=0, e=0, i=0, o=0, u=0, y=0, voyelles=0;
+        String phrase=cl.nextLine();
+        cl.close();
+        for(int b=0; b<phrase.length(); ++b) {
+            if ((phrase.charAt(b) == 'a') || (phrase.charAt(b) == 'A')) {
+                a += 1;
+            }
+            if ((phrase.charAt(b) == 'e') || (phrase.charAt(b) == 'E')) {
+                e += 1;
+            }
+            if ((phrase.charAt(b) == 'i') || (phrase.charAt(b) == 'I')) {
+                i += 1;
+            }
+            if ((phrase.charAt(b) == 'o') || (phrase.charAt(b) == 'O')) {
+                o += 1;
+            }
+            if ((phrase.charAt(b) == 'u') || (phrase.charAt(b) == 'U')) {
+                u += 1;
+            }
+            if ((phrase.charAt(b) == 'y') || (phrase.charAt(b) == 'Y')) {
+                y += 1;
+            }
+        }
+            if(a!=0){
+                System.out.println("a :"+a);
+                voyelles+=a;
+            }
+            if(e!=0){
+                System.out.println("e :"+e);
+                voyelles+=e;
+            }
+            if(i!=0){
+                System.out.println("i :"+i);
+                voyelles+=i;
+            }
+            if(o!=0){
+                System.out.println("o :"+o);
+                voyelles+=o;
+            }
+            if(u!=0){
+                System.out.println("u :"+u);
+                voyelles+=u;
+            }
+            if(y!=0){
+                System.out.println("y :"+y);
+                voyelles+=y;
+            }
+            System.out.println("Le nombre total de voyelle est de: "+voyelles);
+    }
 }
