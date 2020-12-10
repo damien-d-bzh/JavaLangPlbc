5fe86119d7f1aa3285a7b9904a5839e8325a4c1b
damien-d-bzh <51257374+damien-d-bzh@users.noreply.github.com>
2020-12-10 15:09:56 +0100

    Appli qui calcul la distance entre deux point successifs et un peu plus
    Appli qui calcul la distance entre deux point successifs et enfin entre le depart et la fin

--- DistanceSeveralPoints.java
+++ DistanceSeveralPoints.java
@@ -8,21 +8,28 @@ public class DistanceSeveralPoints{
     }
 
     public static float ordonnee(Scanner cl, int i){
-        System.out.print("Entre l'abscisse du point "+(i+1)+" :");
+        System.out.print("Entre l'ordonnée du point "+(i+1)+" :");
         float ordon=cl.nextFloat();
         return ordon;
     }
 
-    public static double distance(float absciD, float absciF, float ordonnD, float ordonnF){
-        double SqrtAbsc=Math.pow(((double)absciF-(double)absciD),(double)2.0);
-        double SqrtOrdon=Math.pow(((double)ordonnF-(double)ordonnD), (double)2.0);
+    public static double distance(float absci[], float ordonn[], int i){
+        double SqrtAbsc=Math.pow(((double)absci[i+1]-(double)absci[i]), (double)2.0);
+        double SqrtOrdon=Math.pow(((double)ordonn[i+1]-(double)ordonn[i]),(double)2.0);
         double distancedf=Math.sqrt(SqrtAbsc+SqrtOrdon);
         //System.out.print("Distance between the two points : "+distancedf);
         return distancedf;
     }
 
+    public static double distanceFirstlastPoint(float absci[], float ordonn[], int i){
+        double SqrtAbscFL=Math.pow(((double)absci[0]-(double)absci[i-1]), (double)2.0);
+        double SqrtOrdonFL=Math.pow(((double)ordonn[0]-(double)ordonn[i-1]),(double)2.0);
+        double distanceFLPoint=Math.sqrt(SqrtAbscFL+SqrtOrdonFL);
+        return distanceFLPoint;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Scanner cl=new Scanner(System.in);
         System.out.print("Entrer le nombre de points à prendre en compte: ");
         int nbrsPoints=cl.nextInt();
@@ -36,13 +43,15 @@ public class DistanceSeveralPoints{
             //System.out.println(ordonn[i]);
         }
         for(int i=0; i<(nbrsPoints-1); ++i){
-            float absciD=absci[i];
-            float absciF=absci[i+1];
-            float ordonnD=ordonn[i];
-            float ordonnF=ordonn[i+1];
-            System.out.println("La distance entre les deux point i et i+1: "+distance(absciD, absciF, ordonnD, ordonnF));
-            distance+=distance(absciD, absciF, ordonnD, ordonnF);
-            System.out.println("Distance totale :"+distance);
+            //float absciD=absci[i];
+            //float absciF=absci[i+1];
+            //float ordonnD=ordonn[i];
+            //float ordonnF=ordonn[i+1];
+            System.out.printf("La distance entre les deux point %d et %d: %.2f \n", i, (i+1), distance(absci, ordonn, i));
+            distance+=distance(absci, ordonn, i);
+            System.out.printf("Distance totale :"+distance);
         }
+        System.out.println("La distance entre le premier et le dernier point est de : "+distanceFirstlastPoint(absci, ordonn, nbrsPoints));
     }
 }
+
